43d33a2f8e768bf3bbe50d76fa1442968791453d
phatdroid94 <phatdroid94@gmail.com>
2020-05-15 20:33:36 +0700

    update ui

--- app/src/main/java/com/example/stringresource30032020/MainActivity.java
+++ app/src/main/java/com/example/stringresource30032020/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.stringresource30032020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
