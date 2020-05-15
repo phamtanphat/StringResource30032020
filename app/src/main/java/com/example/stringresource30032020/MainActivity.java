dfbe3d59af4c9ebeb9f92d2bb0e902d55dfb4c14
phatdroid94 <phatdroid94@gmail.com>
2020-05-15 21:05:08 +0700

    update menu

--- app/src/main/java/com/example/stringresource30032020/MainActivity.java
+++ app/src/main/java/com/example/stringresource30032020/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.stringresource30032020;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,7 +18,21 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
-        return true;
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_language_en:
+                Toast.makeText(this, "En", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_item_language_vn:
+                Toast.makeText(this, "Vn", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        item.setChecked(true);
+        return super.onOptionsItemSelected(item);
     }
 }
